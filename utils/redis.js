aa1627e86ad583b12df6cd67c7c5e411e43ca015
pelumi <olaniyanp11@gmail.com>
2023-10-04 23:08:18 +0000

    corrected typo errors

--- utils/redis.js
+++ utils/redis.js
@@ -1,37 +1,37 @@
 import { createClient } from 'redis';
-import { promisify } from 'utils';
 
-class RedisClient{
-	constructor(){
-		this.__client = createClients();
-		this.__client.on('error', (error) => {
-			console.log(error);
-		})
-	}
+import { promisify } from 'util';
 
-	isAlive(){
-		return this.__client.connected;
-	}
+class RedisClient {
+  constructor() {
+    this.__client = createClient();
+    this.__client.on('error', (error) => {
+      console.log(error);
+    });
+  }
 
-	async get(key){
-		const get = promisify(this.__client).bind(this.__client)
-		const res = await get(key);
-		return res
-	}
+  isAlive() {
+    return this.__client.connected;
+  }
 
-	async set(key, value, duration){
-		const set = promisify( this.__client.set).bind(this.__client);
-		const res = await sets(key, value, 'EX', duration);
-		return res;
-	}
+  async get(key) {
+    const getAsync = promisify(this.__client.get).bind(this.__client);
+    const res = await getAsync(key);
+    return res;
+  }
 
-	async del(key){
-		const del = promisify(this.__client.del).bind(this.__client)
-		const res = await del(key)
-		return res
-	}
+  async set(key, value, duration) {
+    const setAsync = promisify(this.__client.set).bind(this.__client);
+    const res = await setAsync(key, value, 'EX', duration);
+    return res;
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.__client.del).bind(this.__client);
+    const res = await delAsync(key);
+    return res;
+  }
 }
-
 
 const redisClient = new RedisClient();
 export default redisClient;
